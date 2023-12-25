bdee8cebd893cd09703b0d4d6047974b0eb8378b
ncerzzk <huangcmzzk@gmail.com>
2023-12-26 00:20:15 +0800

    add server and client to start the sim_gz
    Signed-off-by: ncerzzk <huangcmzzk@gmail.com>

--- src/gazebo_sim.rs
+++ src/gazebo_sim.rs
@@ -1,7 +1,11 @@
-use std::{ptr::null, os::raw::c_void, sync::Arc, borrow::BorrowMut, cell::RefCell, time::Duration};
+use std::{ os::raw::c_void, sync::Arc, cell::RefCell, time::Duration};
 
-use gz::msgs::{any::Any, request::Request, world_control::WorldControl};
+use gz::msgs::{any::Any};
+use rpos::hrt::Timespec;
 use rpos::workqueue::{WorkQueue, WorkItem};
+use rpos::lock_step::lock_step_update_time;
+use rpos::module::Module;
+use rpos::ctor::ctor;
 use gz::{msgs::world_stats::WorldStatistics};
 
 
@@ -10,11 +14,15 @@ struct GazeboSim{
     gz_node:RefCell<gz::transport::Node>
 }
 
-fn test(s:WorldStatistics){
-    println!("{}",s.iterations);
-}
-
 impl GazeboSim{
+    fn update_time(s:WorldStatistics){
+        let t = Timespec{
+            nsec:s.sim_time.nsec as i64,
+            sec:s.sim_time.sec
+        };
+        lock_step_update_time(t);
+    }
+
     fn new(wq: &Arc<WorkQueue>)->Arc<Self>{
         let sim = Arc::new_cyclic(|weak|{
             let a = GazeboSim{
@@ -24,13 +32,13 @@ impl GazeboSim{
             a
         });
 
-        assert!(sim.gz_node.borrow_mut().subscribe("/world/default/stats",test) );
+        assert!(sim.gz_node.borrow_mut().subscribe("/world/default/stats",Self::update_time) );
         sim
     }
 
     fn run_steps(self:&Arc<Self>,steps:u64){
         let mut req = gz::msgs::world_control::WorldControl::new();
-        req.pause = true;
+        req.pause = false;
         req.multi_step = steps as u32;
 
         self.gz_node.borrow_mut().request::<gz::msgs::world_control::WorldControl,gz::msgs::boolean::Boolean>("/world/default/control",&req,Duration::from_secs(1));
@@ -41,11 +49,15 @@ fn run(ptr:*mut c_void){
 
 }
 
-pub fn init_gazebo_sim(){
+pub fn init_gazebo_sim(_argc:u32, _argv:*const &str){
     let wq = WorkQueue::new("sim",8192, 98, false);
 
     let sim = GazeboSim::new(&wq);
-    println!("sub!");
-    sim.run_steps(100);
+    println!("gz inited!");
     gz::transport::wait_for_shutdown();
+}
+
+#[ctor]
+fn register(){
+    Module::register("sim_gz", init_gazebo_sim);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
 mod gazebo_sim;
+use std::io::{Read, Write};
+use std::os::unix::net::{UnixStream, UnixListener};
+use std::env::{self};
+use std::fs::{self, remove_file};
+use std::str::SplitWhitespace;
+use rpos::module::Module;
+
 fn main() {
+    let is_server;
+    let args:Vec<String> = env::args().collect();
+    const SOCKET_PATH: &str = "./rpsocket";
+    if args.len()> 1 && args[1] == "--server"{
+        is_server = true;
+    }else{
+        is_server = false;
+    }
+
+    
+    if is_server{
+        _ = remove_file(SOCKET_PATH);
+        let stream = UnixListener::bind(SOCKET_PATH).unwrap();
+        let mut cmd_raw = String::new();
+        for client in stream.incoming(){
+            client.unwrap().read_to_string(&mut cmd_raw).unwrap();
+            let cmd_with_args:Vec<_> = cmd_raw.split_whitespace().collect();
+            assert!(cmd_with_args.len()>=1);
+            Module::get_module(cmd_with_args[0]).execute((cmd_with_args.len() - 1) as u32, cmd_with_args[1..].as_ptr());
+            println!("Client said: {}",cmd_raw);
+        }
+    }else{
+        let mut stream = UnixStream::connect(SOCKET_PATH).unwrap(); // panic if the server is not runing.
+        let other_args = args[1..].join(" ");
+        stream.write_all(other_args.as_bytes()).unwrap();
+    }
+
     println!("Hello, world!");
 
-    gazebo_sim::init_gazebo_sim();
+    //gazebo_sim::init_gazebo_sim();
 }
