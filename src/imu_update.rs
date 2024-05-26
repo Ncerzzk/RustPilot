bbb5700377ad5f9a100918329e830731dbd24dde
ncerzzk <huangcmzzk@gmail.com>
2024-05-26 21:56:43 +0800

    add test for imu_update
    Signed-off-by: ncerzzk <huangcmzzk@gmail.com>

--- src/imu_update.rs
+++ src/imu_update.rs
@@ -1,88 +1,95 @@
-use std::{
-    ffi::c_void,
-    ptr::null_mut,
-    sync::Arc,
-};
-
+use std::{ffi::c_void, ptr::null_mut, sync::Arc};
 
 use rpos::{
     channel::{Receiver, Sender},
-    pthread_scheduler::SchedulePthread, msg::{get_new_rx_of_message, get_new_tx_of_message},
+    msg::{get_new_rx_of_message, get_new_tx_of_message},
+    pthread_scheduler::SchedulePthread,
 };
 
-use quaternion_core::{Quaternion as Q, normalize};
+use quaternion_core::{normalize, Quaternion as Q};
 
-use crate::{
-    msg_define::{AccMsg, AttitudeMsg, GyroMsg},
-};
+use crate::msg_define::{AttitudeMsg, Vector3Msg};
 
 use rpos::libc::c_long;
 
 struct IMUUpdate {
-    gyro_rx: Receiver<GyroMsg>,
-    acc_rx: Receiver<AccMsg>,
+    q: Q<f32>,
+    imu_update_ki: f32,
+    imu_update_kp: f32,
 }
 
+impl IMUUpdate {
+    fn update(&mut self, acc: [f32; 3], mut gyro_data: [f32; 3], dt: f32) {
+        let imu_update_half_t = dt / 2.0;
+
+        let acc_normed = quaternion_core::normalize(acc);
+
+        let acc_rotate = quaternion_core::frame_rotation(self.q, [0.0, 0.0, 1.0]);
+        let mut err = quaternion_core::cross(acc_normed, acc_rotate); // use the product of cross as the err
+        let mut err_i: [f32; 3] = [0.0; 3];
+
+        for (index, err_item) in err.iter_mut().enumerate() {
+            if err_item.is_normal() {
+                err_i[index] += (*err_item) * dt * self.imu_update_ki;
+                *err_item = (*err_item) * self.imu_update_kp + err_i[index];
+
+                gyro_data[index] += *err_item;
+            }
+        }
+
+        let q = &mut self.q;
+
+        q.0 += (-q.1[0] * gyro_data[0] - q.1[1] * gyro_data[1] - q.1[2] * gyro_data[2])
+            * imu_update_half_t;
+        q.1[0] += (q.0 * gyro_data[0] + q.1[1] * gyro_data[2] - q.1[2] * gyro_data[1])
+            * imu_update_half_t;
+        q.1[1] += (q.0 * gyro_data[1] - q.1[0] * gyro_data[2] + q.1[2] * gyro_data[0])
+            * imu_update_half_t;
+        q.1[2] += (q.0 * gyro_data[2] + q.1[0] * gyro_data[1] - q.1[1] * gyro_data[0])
+            * imu_update_half_t;
+
+        *q = normalize(*q);
+    }
+}
 
 fn imu_update_main(ptr: *mut c_void) -> *mut c_void {
     let sp = unsafe { Arc::from_raw(ptr as *const SchedulePthread) };
-    let mut wrapper = IMUUpdate {
-        gyro_rx: get_new_rx_of_message::<GyroMsg>("gyro").unwrap(),
-        acc_rx: get_new_rx_of_message::<AccMsg>("acc").unwrap(),
+    let mut gyro_rx = get_new_rx_of_message::<Vector3Msg>("gyro").unwrap();
+    let mut acc_rx = get_new_rx_of_message::<Vector3Msg>("acc").unwrap();
+    let mut imu_update = IMUUpdate {
+        q: (1.0, [0.0; 3]),
+        imu_update_ki: 0.01,
+        imu_update_kp: 2.0,
     };
 
     let q_tx: Sender<AttitudeMsg> = get_new_tx_of_message("attitude").unwrap();
     let mut q_rx_debug: Receiver<AttitudeMsg> = get_new_rx_of_message("attitude").unwrap();
 
     const IMU_UPDATE_T: f32 = 0.002;
-    const IMU_UPDATE_HALF_T: f32 = IMU_UPDATE_T / 2.0;
     const IMU_UPDATE_T_US: c_long = (IMU_UPDATE_T * 1000.0 * 1000.0) as c_long;
-    const IMU_P: f32 = 2.0;
-    const IMU_I: f32 = 0.01;
 
     let mut acc_data: [f32; 3] = [0.0; 3];
     let mut gyro_data: [f32; 3] = [0.0; 3];
-    let mut q: Q<f32> = (1.0, [0.0, 0.0, 0.0]);
 
-    let mut err_i: [f32; 3] = [0.0; 3];
     loop {
-        if let Some(acc_msg) = wrapper.acc_rx.try_read() {
+        if let Some(acc_msg) = acc_rx.try_read() {
             acc_data = [acc_msg.x, acc_msg.y, acc_msg.z];
         }
-        if let Some(gyro_msg) = wrapper.gyro_rx.try_read() {
+        if let Some(gyro_msg) = gyro_rx.try_read() {
             gyro_data = [gyro_msg.x, gyro_msg.y, gyro_msg.z];
         }
-
-        let acc_normed = quaternion_core::normalize(acc_data);
-
-        let acc_rotate = quaternion_core::frame_rotation(q, [0.0, 0.0, 1.0]);
-
-        let mut err = quaternion_core::cross(acc_normed, acc_rotate); // use the product of cross as the err
-        for (index, err_item) in err.iter_mut().enumerate() {
-            if err_item.is_normal() {
-                err_i[index] += (*err_item) * IMU_UPDATE_T * IMU_I;
-                *err_item = (*err_item) * IMU_P + err_i[index];
-
-                gyro_data[index] += *err_item;
-            }
-        }
-
-        q.0 += (-q.1[0] * gyro_data[0] - q.1[1] * gyro_data[1] - q.1[2] * gyro_data[2])
-            * IMU_UPDATE_HALF_T;
-        q.1[0] += (q.0 * gyro_data[0] + q.1[1] * gyro_data[2] - q.1[2] * gyro_data[1])
-            * IMU_UPDATE_HALF_T;
-        q.1[1] += (q.0 * gyro_data[1] - q.1[0] * gyro_data[2] + q.1[2] * gyro_data[0])
-            * IMU_UPDATE_HALF_T;
-        q.1[2] += (q.0 * gyro_data[2] + q.1[0] * gyro_data[1] - q.1[1] * gyro_data[0])
-            * IMU_UPDATE_HALF_T;
-
-        q = normalize(q);
-        let mut x = AttitudeMsg{w:0.0,x:0.0,y:0.0,z:0.0};
-        if let Some(msg) = q_rx_debug.try_read(){
+        imu_update.update(acc_data, gyro_data, IMU_UPDATE_T);
+        let mut x = AttitudeMsg {
+            w: 0.0,
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        if let Some(msg) = q_rx_debug.try_read() {
             x = msg;
         }
         //let x_q = quaternion_core::mul(q,quaternion_core::from_axis_angle([0.0, 0.0, 1.0], -3.14159 / 2.0));
-        println!("cal:{:?} , gazebo:{:?}",q,x);
+        println!("cal:{:?} , gazebo:{:?}", imu_update.q, x);
         // q_tx.send(AttitudeMsg {
         //     w: q.0,
         //     x: q.1[0],
@@ -95,10 +102,43 @@ fn imu_update_main(ptr: *mut c_void) -> *mut c_void {
 }
 
 pub fn init_imu_update(_argc: u32, _argv: *const &str) {
-    SchedulePthread::new(1024*1024, 97, imu_update_main, null_mut(), false, None); // TODO edit pthread_key
+    SchedulePthread::new(1024 * 1024, 97, imu_update_main, null_mut(), false, None);
+    // TODO edit pthread_key
 }
 
 #[rpos::ctor::ctor]
 fn register() {
     rpos::module::Module::register("imu_update", init_imu_update);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::f32::consts::PI;
+
+    use super::IMUUpdate;
+    use quaternion_core::RotationSequence::*;
+
+    #[test]
+    fn test_imu_update() {
+        let mut imu_update = IMUUpdate {
+            q: (1.0, [0.0, 0.0, 0.0]),
+            imu_update_ki: 0.0,
+            imu_update_kp: 0.0,
+        };
+
+        let target_rad = 90.0 / 180.0 * PI;
+        let angle_speed = target_rad / 1.0;
+        for _ in 0..500 {
+            imu_update.update([0.0, 0.0, 1.0], [angle_speed, 0.0, 0.0], 1.0 / 500.0);
+            println!("{:?}", imu_update.q);
+            println!(
+                "angle:{:?}",
+                quaternion_core::to_euler_angles(
+                    quaternion_core::RotationType::Intrinsic,
+                    YXZ,
+                    imu_update.q
+                )
+            );
+        }
+    }
 }
